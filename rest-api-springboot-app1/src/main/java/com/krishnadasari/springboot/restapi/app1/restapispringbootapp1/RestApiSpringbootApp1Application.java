268b2a1e12d61f4691b0bb280ca86bde3b5b6415
dasarikrishnavardhan <dasarikrishnavardhan@outlook.com>
2019-08-21 12:57:06 +0530

    initial commit of project files

---
+++ rest-api-springboot-app1/src/main/java/com/krishnadasari/springboot/restapi/app1/restapispringbootapp1/RestApiSpringbootApp1Application.java
@@ -0,0 +1,13 @@
+package com.krishnadasari.springboot.restapi.app1.restapispringbootapp1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiSpringbootApp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiSpringbootApp1Application.class, args);
+	}
+
+}
